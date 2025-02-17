310b64d8764f48f5034ed13258e6dadc4eb07941
BernatDev6 <bernifogi@gmail.com>
2025-02-17 20:38:50 +0100

    trying git commands

--- frontend_react/src/components/NotFoundPage/NotFoundPage.tsx
+++ frontend_react/src/components/NotFoundPage/NotFoundPage.tsx
@@ -4,7 +4,6 @@ export const NotFoundPage = () => {
     return (
         <div className="not-found-container">
             <h1 className="not-found-title">404 - Página no encontrada</h1>
-            <h1 className="not-found-title">222 - Página no encontrada</h1>
             <p className="not-found-text">
                 Lo sentimos, la página que estás buscando no existe.
             </p>
